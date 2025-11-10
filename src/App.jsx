1247e5e0507fe33c0da3ae7c7d0a6f02cc2d1e04
Igor Montovani <igor.montovani@gmail.com>
2025-11-10 15:35:57 +0100

    small adjustment color of body

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css'
 import NavbarSite from './components/NavbarSite'
 import {Routes,Route} from 'react-router-dom'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <>
       <NavbarSite />
+      <Sidebar />
       <Routes>
         
       </Routes>
