54fe54aee0627c162441c9b2651daf3b54c8ae33
Igor Montovani <igor.montovani@gmail.com>
2025-11-10 15:55:11 +0100

    Add footer to the Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,38 +1,49 @@
+import LogoImg from "../assets/Images/beatmoregames_logo.png";
 
 function Sidebar() {
-    const styleDivSidebar = {
-        backgroundColor: '#1E1E1E',
-        borderRight: '1px solid #333',
-        width: '200px',
-        height: '100vh',
-        display:'flex',
-        flexDirection:'column'
-
-    }
+  const styleDivSidebar = {
+    backgroundColor: "#1E1E1E",
+    borderRight: "1px solid #333",
+    width: "200px",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  };
   return (
     <div style={styleDivSidebar}>
-        <p style={{paddingLeft:'40px', fontSize:'25px',fontWeight:'800'}}>Home</p>
+      <div>
+        <p style={{ paddingLeft: "40px", fontSize: "25px", fontWeight: "800", cursor: 'pointer' }}>
+          Home
+        </p>
+
         <ul className="list-sidebar">
-            <p>Dashboard</p>
-            <li>Profile</li>
-            <li>Games Finished</li>
-            <li>Wishlist</li>
-            <li>Recommended Games</li>
+          <p>Dashboard</p>
+          <li>Profile</li>
+          <li>Games Finished</li>
+          <li>Wishlist</li>
+          <li>Recommended Games</li>
         </ul>
         <ul className="list-sidebar">
-            <p>Top Games</p>
-            <li>Top This Year</li>
-            <li>Top 2024</li>
+          <p>Top Games</p>
+          <li>Top This Year</li>
+          <li>Top 2024</li>
         </ul>
         <ul className="list-sidebar genre">
-            <p>Genres</p>
-            <li>Action</li>
-            <li>RPG</li>
-            <li>Roguelike</li>
-            <li>RTS</li>
+          <p>Genres</p>
+          <li>Action</li>
+          <li>RPG</li>
+          <li>Roguelike</li>
+          <li>RTS</li>
         </ul>
+      </div>
+      <div className="footer-sidebar" style={{paddingTop: '10px',borderTop: '1px solid #333',display:'flex',flexDirection:'column', justifyContent:'center',alignItems:'center'}}>
+        <img style={{ width: "38%" }} src={LogoImg} alt="" />
+        <p>About us</p>
+        <p>Github Project</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
