96452e35c559d7a18e14a4c2085286eb61f337ac
Igor Montovani <igor.montovani@gmail.com>
2025-11-10 17:22:06 +0100

    Add the Routes to the Pages and the container for each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-
-import './App.css'
-import NavbarSite from './components/NavbarSite'
-import {Routes,Route} from 'react-router-dom'
-import Sidebar from './components/Sidebar'
+import "./App.css";
+import NavbarSite from "./components/NavbarSite";
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import AllGames from "./pages/AllGames";
+import Homepage from "./pages/Homepage"
 
 function App() {
-
+  const mainDivStyle = {
+      display: 'flex',
+      justifyContent: 'flex-start',
+      gap:'20px'
+  }
+  const appContainerStyle = {
+        width: '76%',
+        margin: '30px auto',
+    }
   return (
     <>
       <NavbarSite />
-      <Sidebar />
-      <Routes>
-        
-      </Routes>
+      <div style={mainDivStyle}>
+        <Sidebar />
+        <div style={appContainerStyle}>
+          <Routes>
+              <Route path="/" element={<Homepage/>}></Route>
+              <Route path="/all-games" element={<AllGames />}></Route>
+          </Routes>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavbarSite.jsx
+++ src/components/NavbarSite.jsx
@@ -1,5 +1,5 @@
+import { Link } from 'react-router-dom'
 import LogoImg from '../assets/Images/beatmoregames_logo.png'
-
 function NavbarSite() {
     const divNavStyle = {
         backgroundColor: '#171717',
@@ -15,7 +15,7 @@ function NavbarSite() {
                 <img style={{width:'100%'}} src={LogoImg} alt="" />
             </div>
             <div className='pages-navbar' style={{display:'flex',gap:'20px',marginRight:'15px',marginTop:'2px',fontSize:'18px',fontWeight:'400'}}>
-                <p>Games To Play</p>
+                <Link to ='/all-games'><p>Games To Play</p></Link>
                 <p>Your Games</p>
                 <p>Games Finished</p>
             </div>   
