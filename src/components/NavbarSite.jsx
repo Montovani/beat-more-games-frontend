b86289f3dd49b1047df34f500e4d03f6732b8a35
Igor Montovani <igor.montovani@gmail.com>
2025-11-10 14:40:54 +0100

    Add Navbar

---
+++ src/components/NavbarSite.jsx
@@ -0,0 +1,27 @@
+import LogoImg from '../assets/Images/beatmoregames_logo.png'
+
+function NavbarSite() {
+    const divNavStyle = {
+        backgroundColor: '#171717',
+        height: '65px',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+    }
+  return (
+    <>
+        <div style={divNavStyle}>
+            <div style={{width:'100px', marginTop:'10px',marginLeft:'15px'}}>
+                <img style={{width:'100%'}} src={LogoImg} alt="" />
+            </div>
+            <div style={{display:'flex',gap:'20px',marginRight:'15px',marginTop:'2px',fontSize:'18px',fontWeight:'400'}}>
+                <p>Games To Play</p>
+                <p>Your Games</p>
+                <p>Games Finished</p>
+            </div>   
+        </div>
+    </>
+  )
+}
+
+export default NavbarSite
