f5d234404eb6ec8fef700af834209e81110e87e7
Igor Montovani <igor.montovani@gmail.com>
2025-11-13 10:31:24 +0100

    Completelly finished the search and sort by categories filter

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,10 +6,17 @@ import { useState } from "react";
 import axios from "axios";
 
 function Search({gamesList,setGamesList}) {
-  const [search, setSearch] = useState(null)
-  const [platform, setPlatform] = useState(null)
-  const [genre, setGenre] = useState(null)
-
+  const [search, setSearch] = useState("")
+  const [platform, setPlatform] = useState('Platform')
+  const [genre, setGenre] = useState('Genre')
+  const [publisher, setPublisher] = useState('Publisher')
+ 
+  const searchContainer = {
+    display:'flex',
+    flexWrap:'wrap',
+    alignItems:'center',
+    gap:'8px'
+  }
 
   const handleSearchInApi = async()=> {
     try {
@@ -24,8 +31,28 @@ function Search({gamesList,setGamesList}) {
       console.log(error)
     }
   }
+  const handleRefreshSearch = async()=>{
+    const search = ""
+    setSearch(search)
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_API_GAMES_SERVER_URL}/games?key=${import.meta.env.VITE_API_GAMES_KEY}&search=${search}`)
+      
+      const getOnlyEssentials = response.data.results.map((eachGame)=>{
+            const game = {name:eachGame.name,image:eachGame.background_image, slug: eachGame.slug, gameApiId:eachGame.id}
+            return game
+    })
+    setGamesList(getOnlyEssentials)
+    } catch (error) {
+      console.log(error)
+    } 
+   
+  }
 const handlePlatformChange = async(e)=>{
+  setGenre('Genre')
+  setPublisher('Publisher')
+  setSearch("")
   try {
+     setPlatform(e.target.value)
      const platformId = e.target.value === 'ps5' ? '187' 
                      : e.target.value === 'nintendo switch' ? '7' 
                      : e.target.value === 'xbox'? '186'
@@ -40,15 +67,19 @@ const handlePlatformChange = async(e)=>{
             return game
     })
     setGamesList(getOnlyEssentials)
+    
 
   } catch (error) {
     console.log(error)
   }
 }
 const handleGenreChange = async(e)=>{
+  setPublisher('Publisher')
+  setPlatform('Platform')
+  setSearch("")
   try {
-     const genre = e.target.value
-      console.log(genre)
+    const genre = e.target.value
+    setGenre(genre)
     
     
     const response = await axios.get(`${import.meta.env.VITE_API_GAMES_SERVER_URL}/games?key=${import.meta.env.VITE_API_GAMES_KEY}&genres=${genre}`)
@@ -63,8 +94,12 @@ const handleGenreChange = async(e)=>{
   }
 }
 const handlePublishersChange = async(e)=>{
+  setGenre('Genre')
+  setPlatform('Platform')
+  setSearch("")
   try {
      const publisher = e.target.value
+     setPublisher(publisher)
     const response = await axios.get(`${import.meta.env.VITE_API_GAMES_SERVER_URL}/games?key=${import.meta.env.VITE_API_GAMES_KEY}&publishers=${publisher}`)
     const getOnlyEssentials = response.data.results.map((eachGame)=>{
             const game = {name:eachGame.name,image:eachGame.background_image, slug: eachGame.slug, gameApiId:eachGame.id}
@@ -77,45 +112,50 @@ const handlePublishersChange = async(e)=>{
 }
   return (
     <>
-      <Box
-        sx={{
-          "& > :not(style)": { m: 1, width:{xs:'50%',sm:'45ch',} },
-          "& .MuiOutlinedInput-root": {
-            "& fieldset": {
-              borderColor: "white",
+      <div style={searchContainer}>
+        <Box
+          sx={{
+            "& > :not(style)": { m: 1, width:{xs:'50%',sm:'45ch',} },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "white",
+              },
+              "&:hover fieldset": {
+                borderColor: "#ac2847",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "#ac2847",
+              },
             },
-            "&:hover fieldset": {
-              borderColor: "#ac2847",
+            "& .MuiInputLabel-root": {
+              color: "white",
             },
-            "&.Mui-focused fieldset": {
-              borderColor: "#ac2847",
+            "& .MuiInputBase-input": {
+              color: "white",
             },
-          },
-          "& .MuiInputLabel-root": {
-            color: "white",
-          },
-          "& .MuiInputBase-input": {
-            color: "white",
-          },
-        }}
-      >
-        <TextField
-          className="search-input"
-          id="filled-basic"
-          label="Search Your Game"
-          variant="outlined"
-          onChange={(e)=> setSearch(e.target.value)}
-        />
-      </Box>
+          }}
+        >
+          <TextField
+            className="search-input"
+            id="filled-basic"
+            label="Search Your Game"
+            value={search}
+            variant="outlined"
+            onChange={(e)=> setSearch(e.target.value)}
+          />
+        </Box>
+        <Button onClick={handleSearchInApi} className="search-btn" variant="light">Search</Button>
+        <Button onClick={handleRefreshSearch} className="search-btn" variant="light">Refresh</Button>
+       </div>
       <div style={{display:'flex',flexWrap:'wrap',marginTop:'20px'}}>
-      <Form.Select className="search-selector" onChange={handlePlatformChange}>
+      <Form.Select className="search-selector" onChange={handlePlatformChange} value={platform}>
         <option> Platform</option>
         <option value="ps5">PS5</option>
         <option value="Xbox">Xbox</option>
         <option value="pc">PC</option>
         <option value="nintendo switch">Nintendo Switch</option>
       </Form.Select>
-      <Form.Select className="search-selector-2" onChange={handleGenreChange}>
+      <Form.Select className="search-selector-2" onChange={handleGenreChange} value={genre}>
         <option> Genre</option>
         <option value="action">Action</option>
         <option value="indie">Indie</option>
@@ -126,7 +166,7 @@ const handlePublishersChange = async(e)=>{
         <option value="simulation">Simulation</option>
         <option value="puzzle">Puzzle</option>
       </Form.Select>
-      <Form.Select className="search-selector-3" onChange={handlePublishersChange}>
+      <Form.Select className="search-selector-3" onChange={handlePublishersChange} value={publisher}>
         <option> Publisher</option>
         <option value="electronic-arts">Electronic Arts</option>
         <option value="square-enix">Square Enix</option>
@@ -139,7 +179,7 @@ const handlePublishersChange = async(e)=>{
         <option value="feral-interactive">Feral Interactive</option>
         <option value="capcom">Capcom</option>
       </Form.Select>
-      <Button onClick={handleSearchInApi} className="search-btn" variant="light">Search</Button>
+     
       </div>
 
     </>
